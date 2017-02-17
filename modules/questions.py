0d9f65cfce63e49a058717fdc62d962a9ad8c2ac
Henrik Lidbjörk <henrik.lidbjork@gmail.com>
2017-02-17 18:41:09 +0100

    Add questions class and tentative json encoding.

--- modules/questions.py
+++ modules/questions.py
@@ -7,6 +7,55 @@ import conf
 
 questions = Blueprint('questions', __name__)
 
+
+class Question:
+    """The main poll Question class.
+
+    Attributes:
+        number (int): The index number of the question
+        qtext (str): The question
+        answers (list w/ str): All possible answers to the question
+        results (list w/ lists w/ integers): A matrix where the answers (rows)
+            are mapped to band members (cols), and each position will have an
+            integer (pos or neg) denoting how much each respective answer 
+            affects each member score.
+    """
+    def __init__(self, number, qtext, answers, numof_members, numof_answers):
+        self.number = number
+        self.qtext = qtext
+        self.answers = answers
+        self.results = [[0 for x in range(numof_members)]  # Matrix!
+                        for y in range(numof_answers)]
+        
+    def __str__(self):
+        qstring = 'Question: ' + str(self.number) + \
+                  '\n\"' + self.qtext + '\"\n';
+        for y in range(len(self.results)):
+            qstring = qstring + self.answers[y] + '  '
+            for x in self.results[y]:
+                qstring = qstring + str(x) + ' '
+            qstring = qstring + '\n'
+        return qstring
+        
+    def to_json(self):
+        return {'number': self.number, 'qtext': self.qtext, 
+                'answers': self.answers, 'results': self.results}
+
+
+def fetch_question(qnumber):
+    """Fetch poll question number qnumber from database, make it a Question
+       object and return it."""
+    qobject = Question(qnumber, 'How do you feel?', ['Good', 'Okay', 'Baad'], 7, 3)
+    return qobject
+
+
 @questions.route('/')
 def quiz():
+    q = fetch_question(0)
+    print q
+    print q.to_json()
     return render_template('questions.html')
+
+@questions.route('/question')
+def question():
+    return json.dumps(fetch_question(0).to_json())
--- static/javascript/stonespoll.js
+++ static/javascript/stonespoll.js
@@ -1,5 +1,22 @@
 /* Main Javascript for stonespoll web app */
 
+'use strict';
+
+/**
+ * Shorthand for document.getElementById()
+ */
+function byId(elementId) {
+    return document.getElementById(elementId);
+}
+
+/**
+ * Fetch question numbered qNumber from the backend via JSON and 
+ * put it on the page
+ */
+function getQuestion(qNumber) {
+    
+}
+
 window.onload = function() {
-    document.getElementById('startlink').style.visibility = 'visible';
+    byId('startlink').style.visibility = 'visible';
 };
